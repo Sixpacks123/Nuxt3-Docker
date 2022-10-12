24ecf4feea121ce295af254eddf47af746945266
Heurtault Aubin <38321768+Sixpacks123@users.noreply.github.com>
2022-10-12 09:16:07 +0200

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>Hello world !!! 🤐</p>
+        <a href="https://github.com/Suniron">Show Etienne's Github</a>
+    </div>
+</template>
+
+<script setup lang="ts">
+console.log("NODE_ENV", process.env.NODE_ENV)
+
+</script>
